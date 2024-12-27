810a78d1f97f21e972d50b7eb990aaccb8892966
kopyl <oleggggg1997@gmail.com>
2024-12-27 09:05:08 +0200

    Initial Commit

---
+++ match-template-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  match-template-test
+//
+//  Created by Oleh Kopyl on 27.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ match-template-test/match_template_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  match_template_testApp.swift
+//  match-template-test
+//
+//  Created by Oleh Kopyl on 27.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct match_template_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
