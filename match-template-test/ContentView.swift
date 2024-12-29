45f7789590a4b623f660f30573bb5046c68bd8a4
kopyl <oleggggg1997@gmail.com>
2024-12-29 02:25:56 +0200

    Add photo picker
    Former-commit-id: a0970f7445843257429a5ededa2bd26396b513b2

--- match-template-test/ContentView.swift
+++ match-template-test/ContentView.swift
@@ -1,17 +1,26 @@
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
     @State private var displayImage: UIImage?
+    @State private var selectedItem: PhotosPickerItem? = nil
     
     func findTemplate() {
-        print("Showig image")
+        print("Showing image")
         
-        guard let image = UIImage(named: "1.PNG"),
-                  let templateImage = UIImage(named: "plane.png") else {
-                print("Error loading images")
-                return
-            }
+        // Ensure the user has selected an image before proceeding
+        guard let image = displayImage else {
+            print("No image selected")
+            return
+        }
 
+        // Assuming you have the template image loaded from the app bundle or another source
+        guard let templateImage = UIImage(named: "plane.png") else {
+            print("Error loading template image")
+            return
+        }
+
+        // Call your OpenCVWrapper here to process the selected image
         let result = OpenCVWrapper.matchTemplate(image, template: templateImage)
         
         displayImage = result
@@ -22,13 +31,36 @@ struct ContentView: View {
             if let displayImage = displayImage {
                 Image(uiImage: displayImage)
                     .resizable()
+                    .scaledToFit()
             } else {
                 Text("Image not loaded yet.")
             }
+
+            PhotosPicker(
+                selection: $selectedItem,
+                matching: .images,
+                photoLibrary: .shared()) {
+                    Text("Pick a photo")
+                }
+                .onChange(of: selectedItem) { selectedItem, newItem in
+                    Task {
+                        // Retrieve selected asset
+                        if let selectedItem = newItem {
+                            // Retrieve selected photo
+                            if let data = try? await selectedItem.loadTransferable(type: Data.self),
+                               let image = UIImage(data: data) {
+                                displayImage = image
+                            }
+                        }
+                    }
+                }
+
             Button(action: findTemplate) {
-                    Text("Find")
+                Text("Find Template")
             }
+            .padding(.top, 20)
         }
+        .padding()
     }
 }
 
